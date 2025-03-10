ff7190910109036b22549732f4a9b4765813e41a
NursultanMD <NursultanMD@mail.ru>
2025-03-10 16:58:37 +0000

    a

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - Prints a message using the puts function.
+ *
+ * Description: This program prints the sentence
+ * "Programming is like building a multilingual puzzle"
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
 
 
