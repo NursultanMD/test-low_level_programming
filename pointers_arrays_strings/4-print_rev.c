46d334cf4e8cc3f4e03c6e0a4a7bbd067ceab43e
NursultanMD <nursultanmd@mail.ru>
2025-04-07 08:30:05 +0000

    R

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,20 +8,20 @@
  */
 void print_rev(char *s)
 {
-    int len = 0;
+	int len = 0;
 
-    /* Находим длину строки */
-    while (s[len] != '\0')
-    {
-        len++;
-    }
+	/* Находим длину строки */
+	while (s[len] != '\0')
+	{
+		len++;
+	}
 
-    /* Печатаем строку в обратном порядке */
-    while (len > 0)
-    {
-        len--;
-        _putchar(s[len]);
-    }
+	/* Печатаем строку в обратном порядке */
+	while (len > 0)
+	{
+		len--;
+		_putchar(s[len]);
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
