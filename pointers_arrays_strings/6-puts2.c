a2831ae560c0f95b174034df740b748185eb1bbf
NursultanMD <nursultanmd@mail.ru>
2025-04-07 10:15:46 +0000

    A

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string, starting with the first.
+ * @str: The string to be printed.
+ *
+ * Return: Nothing
+ */
+void puts2(char *str)
+{
+    int i = 0;
+
+    /* Проходим по всей строке */
+    while (str[i] != '\0')  
+    {
+        if (i % 2 == 0)  /* Выводим символы на четных индексах */
+            _putchar(str[i]);
+        i++;
+    }
+    _putchar('\n');  /* Печатаем новую строку после завершения */
+}
+
