9dc11739e9c8ac490558a07340e80d614e065073
NursultanMD <nursultanmd@mail.ru>
2025-04-07 08:00:03 +0000

    A

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * reset_to_98 - обновляет значение целого числа, на которое указывает указатель, на 98
- * @n: указатель на целое число
+ * reset_to_98 - Updates the value pointed to by a pointer to 98.
+ * @n: Pointer to an int.
+ *
+ * Return: Nothing.
  */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
