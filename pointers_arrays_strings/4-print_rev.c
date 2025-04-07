c031af776fdf88a043f22ec1ba78d4930a9d174d
NursultanMD <nursultanmd@mail.ru>
2025-04-07 08:28:46 +0000

    R

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse, followed by a new line
+ * @s: Pointer to the string
+ *
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+    int len = 0;
+
+    /* Находим длину строки */
+    while (s[len] != '\0')
+    {
+        len++;
+    }
+
+    /* Печатаем строку в обратном порядке */
+    while (len > 0)
+    {
+        len--;
+        _putchar(s[len]);
+    }
+
+    _putchar('\n');
+}
